e696657136031d5b76451a656a9e50a38c306d9c
PabloMaidana <pablomaid1@gmail.com>
2024-03-20 18:19:13 -0300

    este es mi segundo commit

--- TP2_LAB/src/tp2_lab/TP2_LAB.java
+++ TP2_LAB/src/tp2_lab/TP2_LAB.java
@@ -14,7 +14,7 @@ public class TP2_LAB {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("este es mi segundo commit");
     }
     
 }
